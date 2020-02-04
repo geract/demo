ecba1f7ce19cee333becb05aa8a9783765783ec2
Gerardo Castro <gerardo_ct@hotmail.com>
2020-02-03 19:47:56 -0600

    first commit

---
+++ test/test_helper.rb
@@ -0,0 +1,40 @@
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/reporters'
+require 'capybara/rails'
+require 'capybara/minitest'
+require 'database_cleaner'
+require 'database_cleaner_support'
+
+MiniTest::Reporters.use!
+DatabaseCleaner.clean_with :truncation
+DatabaseCleaner.strategy = :transaction
+
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+end
+
+class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
+  include DatabaseCleanerSupport
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+  include DatabaseCleanerSupport
+
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
