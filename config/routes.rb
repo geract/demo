cd7ae7cf70409fc37a0c73b3e291e95bc9f6f6af
Nataly Silva <mildred_natali@hotmail.com>
2020-02-06 13:21:02 -0600

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins, path: 'admin', 
     controllers: { 
       sessions: "admins/sessions",
-      registrations: 'users/registrations'
+      registrations: 'admins/registrations'
     }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
