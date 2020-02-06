550aa1a42b52750a8e4238280f4cf2476ce2c04b
Nataly Silva <mildred_natali@hotmail.com>
2020-02-06 12:51:24 -0600

    Add devise for admin Generate views, controllers, routes and model for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins, path: 'admin', 
+    controllers: { 
+      sessions: "admins/sessions",
+      registrations: 'users/registrations'
+    }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
