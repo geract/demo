24b650b5a118ae02f4d0c18c67c219af72522815
Gerardo Castro <gerardo_ct@hotmail.com>
2020-04-09 10:42:30 -0500

    fix for search on rescue groups api

--- lib/pets/search/rescue_groups/params_formater.rb
+++ lib/pets/search/rescue_groups/params_formater.rb
@@ -7,14 +7,15 @@ class Pets::Search::RescueGroups::ParamsFormater
   
   def execute
     return {} if params.blank?
-    {
-      id: params[:id],
-      location: params[:zipCode],
-      location_distance: params[:radius],
-      breed: params[:breed],
-      general_size_potential: params[:size],
-      sex: params[:sex], 
-      general_age: params[:age]
-    }.compact
+
+    rg_params = {}
+    rg_params[:id] = params[:id] if params[:id]
+    rg_params[:location] = params[:zipCode] if params[:zipCode]
+    rg_params[:location_distance] = params[:radius] if params[:radius] && params[:zipCode]
+    rg_params[:breed] = params[:breed] if params[:breed]
+    rg_params[:general_size_potential] = params[:size] if params[:size]
+    rg_params[:sex] = params[:sex] if params[:sex]
+    rg_params[:age] = params[:age] if params[:age]
+    rg_params
   end
 end
