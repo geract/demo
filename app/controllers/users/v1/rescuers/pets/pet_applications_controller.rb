99f75327cd631d8fa8c8a524e4066ee63b0d0707
JuanBrizuelaC <juan@petparenthub.com>
2020-04-30 18:05:42 -0500

    Add favorites filter to Rescuer::Pets::PetApplication controller

--- app/controllers/users/v1/rescuers/pets/pet_applications_controller.rb
+++ app/controllers/users/v1/rescuers/pets/pet_applications_controller.rb
@@ -1,11 +1,25 @@
 class Users::V1::Rescuers::Pets::PetApplicationsController < Users::V1::Rescuers::BaseController
   def index
-    pet = current_organization.pets.includes(applications: :adopter_profile).find_by(id: params[:pet_id])
-
+    pet = current_organization.pets.find_by(id: params[:pet_id])
+    
     if pet
-      render json: Rescuers::Pets::PetApplications::IndexPresenter.new(pet.applications), status: :ok
+      applications = unless filter_params.blank?
+        pet.applications.includes(adopter_profile: :address)
+      else
+        pet.applications.includes(adopter_profile: :address).where(filter_params)
+      end
+  
+      render json: Rescuers::Pets::PetApplications::IndexPresenter.new(applications), status: :ok
     else
       head :unprocessable_entity
     end
+  end
+
+  private
+
+  def filter_params
+    return {} unless params[:filters]
+
+    params.require(:filters).permit(:favorite)
   end
 end
--- test/controllers/users/v1/rescuers/pets/pet_applications_controller_test.rb
+++ test/controllers/users/v1/rescuers/pets/pet_applications_controller_test.rb
@@ -13,7 +13,28 @@ class Users::V1::Rescuers::Pets::PetApplicationsControllerTest < ActionDispatch:
     create(:pet_application, pet: @pet)
 
     get rescuers_pet_pet_applications_path(pet_id: @pet.id),
-    headers: headers_v1(@user.uid, @credentials.token, @credentials.client)
+      headers: headers_v1(@user.uid, @credentials.token, @credentials.client)
+
+    api_response = JSON.parse(response.body)
+
+    assert_response :success
+    assert api_response.any?
+    assert api_response[0]['id'].present?
+    assert api_response[0]['adopter'].present?
+    assert_equal api_response[0]['adopter']['first_name'], 'Ale'
+    assert_equal api_response[0]['adopter']['last_name'], 'Jandra'
+    assert api_response[0]['adopter']['address_attributes'].present?
+    assert_equal api_response[0]['adopter']['address_attributes']['city'], 'Mountain View'
+    assert_equal api_response[0]['adopter']['address_attributes']['state'], 'CA'
+  end
+
+  def test_index_success_with_filters
+    @pet = create(:pet, organization: @organization, name: 'Pet1', added_by: @user)
+    create(:pet_application, pet: @pet, favorite: true)
+
+    get rescuers_pet_pet_applications_path(pet_id: @pet.id),
+      params: {filters: { favorite: true }},
+      headers: headers_v1(@user.uid, @credentials.token, @credentials.client)
 
     api_response = JSON.parse(response.body)
 
