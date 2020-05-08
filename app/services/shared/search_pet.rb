a957d2bebef2fd9752d0beced2d296898d7a8dcb
Gerardo Castro <gerardo_ct@hotmail.com>
2020-05-08 15:07:31 -0500

    add fix to pet seacher service

--- app/services/shared/search_pet.rb
+++ app/services/shared/search_pet.rb
@@ -1,7 +1,7 @@
 class Shared::SearchPet
   def self.perform(args)
-    external_source = Pets::Search::RescueGroups.new.execute(args)
     local_source = Pets::Search::Database.new.execute(args)
+    external_source = Pets::Search::RescueGroups.new.execute(args)
 
     local_source.to_a.concat(external_source)
   end
