398e1287150e0e9aece14f667d68dc743e87c8f0
JuanBrizuelaC <juan@petparenthub.com>
2020-05-08 15:17:51 -0500

    Fixed small bug on presenter

--- app/presenters/users/adopters/profile_presenter.rb
+++ app/presenters/users/adopters/profile_presenter.rb
@@ -32,9 +32,9 @@ class Users::Adopters::ProfilePresenter
       first_name: adopter_profile.first_name,
       last_name: adopter_profile.last_name,
       address_attributes: {
-        id: adopter_profile.address.id,
-        city: adopter_profile.address.city,
-        state: adopter_profile.address.state
+        id: adopter_profile.address&.id,
+        city: adopter_profile.address&.city,
+        state: adopter_profile.address&.state
       }
     }.compact
   end
