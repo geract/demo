7443a91317887b8c381f42bd19a8008d8e38c7da
Gerardo Castro <gerardo_ct@hotmail.com>
2020-05-21 20:43:30 -0500

    update default_url_options host to staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -117,7 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  Rails.application.config.action_mailer[:default_url_options] = { host: 'https://staging.petparenthub.com/' }
+  config.action_mailer.default_url_options = { host: 'https://staging.petparenthub.com/' }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => Rails.application.credentials.dig(:smtp, :user_name),
