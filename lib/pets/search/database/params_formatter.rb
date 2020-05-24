a9b1534fa99540751e24f442525caab75c70f23f
Gerardo Castro <gerardo_ct@hotmail.com>
2020-05-24 17:18:01 -0500

    add slug to params for local database

--- lib/pets/search/database/params_formatter.rb
+++ lib/pets/search/database/params_formatter.rb
@@ -9,9 +9,10 @@ class Pets::Search::Database::ParamsFormatter
     return {} if params.blank?
 
     db_params = {}
-    db_params[:id] = params[:id] unless params[:id].blank?
-    db_params[:status] = params[:status] unless params[:status].blank?
-    db_params[:organization_id] = params[:organization_id] unless params[:organization_id].blank?
+    db_params[:id] = params[:id] if !params[:id].blank?
+    db_params[:slug] = params[:slug] if !params[:slug].blank?
+    db_params[:status] = params[:status] if !params[:status].blank?
+    db_params[:organization_id] = params[:organization_id] if !params[:organization_id].blank?
     db_params
   end
 end
